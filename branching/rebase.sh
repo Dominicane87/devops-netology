337e4f263360706c68dd0d4a5a20b946db4157d4
VladimirGorin <vladimir.gorin@gazprombank.ru>
2020-11-18 21:17:53 +0300

    git-rebase 1

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -2,9 +2,23 @@
 # display command line options
 
 count=1
+<<<<<<< HEAD
+
 for param in $@; do
     echo $@ Parameter #$count = $param
+=======
+for param in "$@"; do
+    echo "Parameter: $param" на echo "Next parameter: $param"
+>>>>>>> 62004f9... git rebase 2
     count=$(( $count + 1 ))
 done
 
-echo =====
+
+
+
+
+
+
+
+
+
